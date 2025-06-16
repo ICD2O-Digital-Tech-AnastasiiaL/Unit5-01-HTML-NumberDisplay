34a106f2f2980c5442d423e56c5132abe1cb2f26
Anastasiia Lebedenko <anastasiia.lebedenko@stu.ocsb.ca>
2025-06-16 14:00:10 +0000

    Loop

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Anastasiia Lebedenko All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Anastasiia Lebedenko
+// Created on: June 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the volume of the triangle
+ */
+function displayNumbers() {
+  // Information about values
+  
+  let numbers = "";
+  let userNumone = parseInt(document.getElementById('userNumone').value);
+  let counter = userNumone;
+  let userNumtwo = parseInt(document.getElementById('userNumtwo').value);
+
+  while (counter <= userNumtwo) { 
+    numbers = numbers + counter + "<br>";
+    counter = counter + 1;
+  }
+
+  // Display
+  document.getElementById('display-results').innerHTML = numbers;
+}
